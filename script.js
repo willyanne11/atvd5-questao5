8e6e1f15b125f36aad406976a2f6c54e5919cb0b
willyanne11 <willyannemayara5@gmail.com>
2024-12-02 19:25:59 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let valorProduto = parseFloat(prompt('Digite um valor de um produto: '));
+
+let reajuste = valorProduto * 0.3333;
+let somaValores = valorProduto + reajuste;
+
+alert('O valor do seu reajuste é de: ' + reajuste)
+alert('O seu saldo com o reajuste é de: ' + somaValores);
